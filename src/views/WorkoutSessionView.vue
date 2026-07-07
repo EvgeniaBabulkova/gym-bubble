423a00bf89c572ea1a690c904555211aa9dedbff
EvgeniaBabulkova <113165892+EvgeniaBabulkova@users.noreply.github.com>
2026-07-08 00:29:14 +0300

    open/close exercise based on sets input

--- src/views/WorkoutSessionView.vue
+++ src/views/WorkoutSessionView.vue
@@ -1,26 +1,46 @@
 <script setup lang="ts">
 import Button from '@/components/UI/Button.vue'
+import TextArea from '@/components/UI/TextArea.vue'
 import { useWorkoutById } from '@/composables/useWoroutById'
+import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
 const workoutId = Number(route.params.workoutId)
 
 const { workout, workoutExercises } = useWorkoutById(workoutId)
+const activeExercises = ref<number[]>([])
+const exerciseSetDrafts = ref<Record<number, string>>({})
+
+// close empty exercises, then make exercise active
+function makeExerciseActive(exerciseId: number) {
+  activeExercises.value = activeExercises.value.filter((id) => exerciseSetDrafts.value[id]?.trim())
+
+  if (!activeExercises.value.includes(exerciseId)) {
+    activeExercises.value.push(exerciseId)
+  }
+}
 
 function finishWorkoutSession() {}
 </script>
 
 <template>
-  <section v-if="workout">
+  <form v-if="workout" @submit.prevent="finishWorkoutSession">
     <h2>You're doing {{ workout.name }}</h2>
     <ul>
       <li v-for="exercise in workoutExercises" :key="exercise.id">
         {{ exercise.name }}
+        <TextArea
+          v-if="activeExercises.includes(exercise.id)"
+          v-model="exerciseSetDrafts[exercise.id]"
+          label="Sets and reps"
+          :rows="2"
+        />
+        <Button v-else type="button" @click="makeExerciseActive(exercise.id)">Add set data</Button>
       </li>
     </ul>
-    <Button @click="finishWorkoutSession">Finish workout</Button>
-  </section>
+    <Button type="submit">Finish workout</Button>
+  </form>
 
   <p v-else>This workout could not be found!</p>
 </template>
